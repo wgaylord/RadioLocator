79f596c3d6bdb46626d397ec03ae8c94381b5064
Chibill <chibill110@gmail.com>
2016-08-18 04:18:24 +0000

    Work alittle more on the Prompt before I go to bed.

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import data
 import convertions
 from cmd import Cmd
+import rtlsdr
 
 class Prompt(Cmd):
     
     def do_collect_data(self,args):
-        """Collects data Location,Freqency and Gain must be give as Args"""
+        """Collects data. Usage collect_data logatude latitude freqency sampleNumber"""
         pass
     
     def do_disableSDR(self,args):
+        """Disables the RTLSDR so that it can safely be unpluged from your comp"""
         pass
     
     def do_enableSDR(self,args):
+        """Enables the RTLSDR so that I/Q Data can be sampled from it"""
         pass
     
     def do_quit(self, args):
